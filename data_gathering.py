094e4878a62034e27a1e0aef53c53205d98b3581
Oleksii Kyrylchuk <olety7@gmail.com>
2017-06-06 21:03:04 +0200

    Greatly improved performance
    Things done:
    - Changed the data gathering algorithm
        - Downside: We have to parse the data twice
        - Upside: It's a lot faster than before due to
            no having the set differences and sorts
    Todo:
    - Improve regex parsing
    - Model training

--- data_gathering.py
+++ data_gathering.py
@@ -4,6 +4,7 @@ import logging
 import os
 import re
 import sys
+import traceback
 from contextlib import contextmanager  # To make a cd function
 
 import numpy as np  # Arrays
@@ -19,7 +20,7 @@ importlib.reload(logging)
 logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s',
                     level=logging.INFO, stream=sys.stdout, datefmt='%H:%M:%S')
 # Numpy printing setup
-np.set_printoptions(threshold=10, linewidth=79, edgeitems=20)
+np.set_printoptions(threshold=10, linewidth=79, edgeitems=5)
 
 # %% Making some helper functions
 
@@ -43,17 +44,17 @@ def cd(dir):  # Must be a generator
         os.chdir(old_dir)
 
 
-def list_dir():
+def list_dir(path=None):
     '''Lists the items in the directory, but ommits hidden files.'''
-    return [item for item in os.listdir() if not item.startswith('.')]
+    return [item for item in os.listdir(path) if not item.startswith('.')]
 
 
 # WORD SELECTION SETUP
 # Only processing 3+ letter words
-WORD_PATTERN = re.compile('[a-z][a-z][a-z]+')
+WORD_PATTERN = re.compile('(?:[a-z][a-z][a-z]+)')
 # Removing "Key: value" stuff
 HEADER_PATTERN = re.compile('^[A-Z][a-z]+([- ]\w+){0,2}:.+$')
-EMAIL_PATTERN = re.compile('\w+?@\w+?\.\w+?')
+EMAIL_PATTERN = re.compile('(?:\w+?@\w+?\.\w+?)')
 
 
 def get_word_dict(filename):
@@ -80,6 +81,21 @@ def get_word_dict(filename):
     return return_dict
 
 
+def get_word_dict_proper(filename, proper_words):
+    return_dict = {word: 0 for word in proper_words}
+    with open(filename, 'r', errors='ignore') as f:
+        linelist = list(f)
+    cleaned_linelist = [EMAIL_PATTERN.sub('', line).lower().strip() for line in linelist
+                        if not line.startswith('*') and not HEADER_PATTERN.match(line)]
+    for line in cleaned_linelist:
+        for word in WORD_PATTERN.findall(line):
+            try:
+                return_dict[word] += 1
+            except KeyError:  # We don't care about other words
+                pass
+    return return_dict
+
+
 def generate_classes(folder_name):
     '''
         Generates the class dictionary given a folder_name.
@@ -93,77 +109,114 @@ def generate_classes(folder_name):
     return_dict = {}
     i = 0
     with cd(folder_name):
-        for data_class in [item for item in os.listdir() if os.path.isdir(item)]:
+        for data_class in [item for item in list_dir() if os.path.isdir(item)]:
             return_dict[data_class] = i
             i += 1
     return return_dict
 
 
-def process_data_folder(folder_name, class_dict, total_word_count=False):
+# Word column names in the dataframe
+def check_df(dataframe, class_dict, df_type='train', coverage_pct=.05):
+    '''
+        Checks whether a dataframe is valid based on random selection of rows
+        Arguments:
+            dataframe (pd.DataFrame): The dataframe to be checked
+            reverse_class_dict (dict): The dictionary {'class_name': number}
+            df_type (string): 'test' or 'train'
+            coverate_pct (float): The percentage of rows to check
+        Returns:
+            bool: True if it's valid, False if it's not
+        Examples:
+            >>> is_valid = check_df(my_df, class_dict, rows_to_check=10)
+            True
+    '''
+    word_columns = dataframe.columns.tolist()[2:]  # This is faster than regex
+    class_dict_rev = {v: k for k, v in class_dict.items()}
+    try:
+        with cd(os.path.join('data', df_type)):
+            for idx, row in dataframe.sample(int(coverage_pct * dataframe.shape[0])).iterrows():
+                with cd(class_dict_rev[row['prediction_class']]):
+                    counts = get_word_dict(str(row['document_id']))
+                    for word in word_columns:
+                        if counts[word] != row[word]:
+                            logging.error('Word {} appears in the file {} times, and is in the df {} times'
+                                          .format(word, counts[word], row[word]))
+                            logging.error('Row:\n{}'.format(row))
+                            return False
+
+    except KeyError:
+        pass
+    except Exception:
+        logging.error(traceback.format_exc())
+        logging.error('Row:\n{}'.format(row))
+        return False
+    return True
+
+
+def get_total_word_count(folder_name):
+    twc_dict = {}
+    logging.info('Getting the total word count in {}'.format(folder_name))
+    with cd(folder_name):
+        for data_class in tqdm([item for item in list_dir() if os.path.isdir(item)],
+                               desc='Classes processed', leave=False,
+                               file=sys.stdout, unit='class'):
+            logging.debug('get_total_word_cound | Processing class "{}"...'
+                          .format(data_class))
+            with cd(data_class):
+                for filename in [item for item in list_dir() if os.path.isfile(item)]:
+                    temp_word_dict = get_word_dict(filename)
+                    # twc_dict: {'word': [total_count, occured_in_N_files]}
+                    for word, count in temp_word_dict.items():
+                        try:
+                            twc_dict[word][0] += count
+                            twc_dict[word][1] += 1
+                        except KeyError:  # First occurence of a word
+                            twc_dict[word] = [count, 1]
+    return twc_dict
+
+
+def process_data_folder(folder_name, class_dict, popular_words):
     '''
     Counts the occurances of a word in a file given filename
     Arguments:
         folder_name (str): The name or path of a data folder you want to process
         class_dict (dict): A dictionary of classes {class_name: class_number}
-        total_word_count (bool, optional): A flag that determines whether you want to
-            return an additional parameter. Defaults to False.
+        popular_words (iterable): An iterable of strings that has the words that
+            we should count.
     Returns:
-        np.array: The column names for every processed file in the class folder
-        np.array: The values corresponding to the column names
-        dict (optional): A dictionary {word_name: (occurence_count, file_count)}.
-            Is only returned when the total_word_count = True
-    Examples:
-        class_dict = generate_classes('train')
-        train_cols, train_vals, twc_train =
-            process_data_folder('train', class_dict, total_word_count=True)
+        np.array(str): The column names for every file
+        np.array(np.array(int)): The array of values corresponding to the column names
     '''
-    return_cols = []
+    # We have the same return_cols for every file
+    return_cols = np.array(['document_id', 'prediction_class'] +
+                           list(popular_words),
+                           dtype=np.dtype('str'))
     return_vals = []
-    if total_word_count:
-        # For selecting k top words
-        twc_dict = {}
+    logging.info('Parsing the folder {}'.format(folder_name))
     with cd(folder_name):
-        for data_class in tqdm([item for item in os.listdir() if os.path.isdir(item)],
+        for data_class in tqdm([item for item in list_dir() if os.path.isdir(item)],
                                desc='Classes processed', leave=False,
-                               file=sys.stdout, unit='classes'):
-            logging.debug('Processing class "{}"...'.format(data_class))
+                               file=sys.stdout, unit='class'):
+            logging.debug('process_data_folder | Processing class "{}"...'
+                          .format(data_class))
             with cd(data_class):
-                for filename in [item for item in os.listdir() if os.path.isfile(item)]:
-                    temp_word_dict = get_word_dict(filename)
-                    # Creating an array of column names
-                    return_cols.append(np.array(['document_id',
-                                                 'prediction_class'] +
-                                                list(temp_word_dict.keys()),
-                                                dtype=np.dtype('str')))
+                for filename in [item for item in list_dir() if os.path.isfile(item)]:
+                    temp_word_dict = get_word_dict_proper(filename,
+                                                          popular_words)
                     # Creating an array of values
-                    return_vals.append(np.array([filename,
-                                                 class_dict[data_class]] +
+                    return_vals.append(np.array([filename, class_dict[data_class]] +
                                                 list(temp_word_dict.values()),
                                                 dtype=np.dtype('int')))
-                    if total_word_count:
-                        # twc_dict: {'word': [total_count, occured_in_N_files]}
-                        for word, count in temp_word_dict.items():
-                            try:
-                                twc_dict[word][0] += count
-                                twc_dict[word][1] += 1
-                            except KeyError:  # First occurence of a word
-                                twc_dict[word] = [count, 1]
-    if total_word_count:
-        return (np.array(return_cols),
-                np.array(return_vals),
-                twc_dict)
     return (np.array(return_cols),
             np.array(return_vals))
 
 
-# %% Reading the files
-
-
-if 'data' not in os.listdir():
+# %% Reading the files - getting the popular words
+if 'data' not in list_dir():
     sys.exit('Could not find the data folder. '
              'Make sure that the notebook is run from the "News-Learning" dir.')
 
-if 'test' not in os.listdir('data') or 'train' not in os.listdir('data'):
+if 'test' not in list_dir('data') or 'train' not in list_dir('data'):
     sys.exit('Bad structure of the data folder. '
              'Make sure that the data folder contains "test" and "train" dirs.')
 
@@ -172,33 +225,15 @@ with cd('data'):
     logging.info('Generating classes...')
     class_dict = generate_classes('train')
 
-    logging.info('Processing train data...')
-    train_cols, train_vals, twc_train = process_data_folder(
-        'train', class_dict, total_word_count=True)
+    logging.info('Getting the total word counts...')
+    twc_train = get_total_word_count('train')
 
-    logging.info('Processing test data...')
-    test_cols, test_vals = process_data_folder('test', class_dict)
-
-logging.info('Train data sample:')
-logging.info('Columns:')
-display(pd.DataFrame(train_cols[0]))
-logging.info('Values:')
-display(pd.DataFrame(train_vals[0]))
-
-logging.info('Test data sample:')
-logging.info('Columns:')
-display(pd.DataFrame(test_cols[0]))
-logging.info('Values:')
-display(pd.DataFrame(test_vals[0]))
-
-
-# %% Transforming the data - selecting the dataframes with k top words
-
+# %% Transforming the data - selecting the most common words
 NUMBER_TOP_WORDS = 10_000
 
-# Making the 2d array
+# Making the 2d array from a dictionary
 wordlist = [[w, v[0], v[1]] for (w, v) in twc_train.items()]
-word_arr = np.vstack(tuple(wordlist))
+word_arr = np.stack(tuple(wordlist), axis=0)  # vertical stack
 
 # Making a df out of it
 word_df = pd.DataFrame(data=word_arr,
@@ -227,94 +262,60 @@ uwords_df = pd.DataFrame(data=uwords_series)
 logging.info('Final dataframe')
 display(uwords_df)
 
+# %% Getting the data - Parsing train and test folders
+
+with cd('data'):
+    logging.info('Getting the train data...')
+    train_cols, train_vals = process_data_folder(
+        'train', class_dict, uwords_df.T.values[0])
+    logging.info('Getting the test data...')
+    test_cols, test_vals = process_data_folder(
+        'test', class_dict, uwords_df.T.values[0])
+
+
+logging.info('Train data sample:')
+logging.info('Columns:')
+display(pd.DataFrame(train_cols).T)
+logging.info('Values:')
+display(pd.DataFrame(train_vals[0]).T)
+
+logging.info('Test data sample:')
+logging.info('Columns:')
+display(pd.DataFrame(test_cols).T)
+logging.info('Values:')
+display(pd.DataFrame(test_vals[0]).T)
+
 
 # %% Transforming the data - Making dataframes
 
-# Used in the transformations
-column_headers = np.concatenate((
-    np.array(['document_id',
-              'prediction_class']),
-    uwords_df.T.values[0])
-).astype(np.dtype('str'))
-
-# Used in the resulting dataframes
-column_headers_sorted = np.sort(column_headers)
-
-# Numpy processing method gave ~10-100x speed boost compared to the dataframe one
-# (from 1-10 DFs per second to ~100 arrays per second)
-
-# Processing the train data
-train_arrs = []
-
-total_len = len(train_cols)
-iter_seq = zip(train_cols, train_vals)
-logging.info('Transforming the train data into an arraylist...')
-for col, val in tqdm(iter_seq, desc='Arrays processed', total=total_len, leave=True, file=sys.stdout, unit='arrs'):
-    # First, sort the col/vals
-    idx = np.argsort(col)
-    col = col[idx]
-    val = val[idx]
-    # Get the set of extra words
-    sd = np.setdiff1d(col, column_headers)
-    # Find the extra words in cols
-    # We sorted them in the start to use this function
-    del_index = np.searchsorted(col, sd)
-    # Get rid of the extra words
-    col = np.delete(col, del_index)
-    val = np.delete(val, del_index)
-    # get the missing words
-    sd = np.setdiff1d(column_headers, col)
-    # horizontal stack with the set of words that we're missing
-    col = np.concatenate((col, sd))
-    val = np.concatenate((val, np.zeros(sd.shape)))
-    # Argsort returns indices that sort an array
-    # then we apply the column sort to the values to preserve order
-    # (alphabetical)
-    train_arrs.append(val[np.argsort(col)])
-
-train_arrs = np.array(train_arrs, dtype='int')
-logging.info('Transforming the train arraylist into a dataframe...')
-train_df = pd.DataFrame(data=train_arrs, columns=column_headers_sorted)
-
-# Processing the test data
-test_arrs = []
-
-total_len = len(test_cols)
-iter_seq = zip(test_cols, test_vals)
-logging.info('Transforming the test data into an arraylist...')
-for col, val in tqdm(iter_seq, desc='Arrays processed', total=total_len, leave=True, file=sys.stdout, unit='arrs'):
-    # First, sort the col/vals
-    idx = np.argsort(col)
-    col = col[idx]
-    val = val[idx]
-    # Get the set of extra words
-    sd = np.setdiff1d(col, column_headers)
-    # Find the extra words in cols
-    # We sorted them in the start to use this function
-    del_index = np.searchsorted(col, sd)
-    # Get rid of the extra words
-    col = np.delete(col, del_index)
-    val = np.delete(val, del_index)
-    # get the missing words
-    sd = np.setdiff1d(column_headers, col)
-    # horizontal stack with the set of words that we're missing
-    col = np.concatenate((col, sd))
-    val = np.concatenate((val, np.zeros(sd.shape)))
-    # Argsort returns indices that sort an array
-    # then we apply the column sort to the values to preserve order
-    # (alphabetical)
-    test_arrs.append(val[np.argsort(col)])
-
-test_arrs = np.array(test_arrs, dtype='int')
-logging.info('Transforming the test arraylist into a dataframe...')
-test_df = pd.DataFrame(data=test_arrs, columns=column_headers_sorted)
-
-
-# Checking the results
+# Train
+logging.info('Making the train dataframe...')
+train_df = pd.DataFrame(data=np.stack(train_vals), columns=train_cols)
 logging.info('Train DF:')
 display(train_df)
+
+# Test
+logging.info('Making the test dataframe...')
+test_df = pd.DataFrame(data=np.stack(test_vals), columns=test_cols)
 logging.info('Test DF:')
 display(test_df)
+
+# Checking
+PCT_TO_CHECK = 0.10
+
+logging.info('Checking the train_df ({} rows): {}!'
+             .format(int(PCT_TO_CHECK * train_df.shape[0]),
+                     'Passed the check'
+                     if check_df(train_df, class_dict, coverage_pct=PCT_TO_CHECK)
+                     else 'Didn\'t pass the check!'))
+
+logging.info('Checking the test_df ({} rows): {}!'
+             .format(int(PCT_TO_CHECK * test_df.shape[0]),
+                     'Passed the check'
+                     if check_df(test_df, class_dict,
+                                 df_type='test', coverage_pct=PCT_TO_CHECK)
+                     else 'Didn\'t pass the check!'))
+
 
 # %% Saving the results
 
