f2079933681ddba30a57bdd67c5102b6b832ef8d
Oleksii Kyrylchuk <olety7@gmail.com>
2017-06-16 16:33:45 +0200

    Data gathering update
    - Added WEBSITE_PATTERN to data_gathering in order to exclude website
    names.

--- data_gathering.py
+++ data_gathering.py
@@ -61,6 +61,8 @@ WORD_PATTERN = re.compile('(?:[a-z][a-z][a-z]+)')
 # Removing "Key: value" stuff
 HEADER_PATTERN = re.compile('^[A-Z][a-z]+([- ]\w+){0,2}:.+$')
 EMAIL_PATTERN = re.compile('(?:\w+?@\w+?\.\w+?)')
+WEBSITE_PATTERN = re.compile('((?:http://){0,1}(?:www.){0,1}(?:[a-z0-9_]+?\.)'
+                             '(?:[a-z0-9_]+(?:\.){0,1}){1,3})')
 
 
 def get_word_dict(filename):
@@ -77,8 +79,11 @@ def get_word_dict(filename):
     return_dict = {}
     with open(filename, 'r', errors='ignore') as f:
         linelist = list(f)
-    cleaned_linelist = [EMAIL_PATTERN.sub('', line).lower().strip() for line in linelist
-                        if not line.startswith('*') and not HEADER_PATTERN.match(line)]
+    cleaned_linelist = [
+        WEBSITE_PATTERN.sub('', EMAIL_PATTERN.sub('', line.lower().strip()))
+        for line in linelist
+        if not line.startswith('*') and not HEADER_PATTERN.match(line)
+    ]
     for line in cleaned_linelist:
         for word in WORD_PATTERN.findall(line):
             try:
@@ -103,8 +108,10 @@ def get_word_dict_proper(filename, proper_words):
     return_dict = {word: 0 for word in proper_words}
     with open(filename, 'r', errors='ignore') as f:
         linelist = list(f)
-    cleaned_linelist = [EMAIL_PATTERN.sub('', line).lower().strip() for line in linelist
-                        if not line.startswith('*') and not HEADER_PATTERN.match(line)]
+    cleaned_linelist = [
+        EMAIL_PATTERN.sub('', line).lower().strip() for line in linelist
+        if not line.startswith('*') and not HEADER_PATTERN.match(line)
+    ]
     for line in cleaned_linelist:
         for word in WORD_PATTERN.findall(line):
             try:
@@ -153,13 +160,15 @@ def check_df(dataframe, class_dict, df_type='train', coverage_pct=.05):
     class_dict_rev = {v: k for k, v in class_dict.items()}
     try:
         with cd(os.path.join('data', df_type)):
-            for idx, row in dataframe.sample(int(coverage_pct * dataframe.shape[0])).iterrows():
+            for idx, row in dataframe.sample(
+                    int(coverage_pct * dataframe.shape[0])).iterrows():
                 with cd(class_dict_rev[row['prediction_class']]):
                     counts = get_word_dict(str(row['document_id']))
                     for word in word_columns:
                         if counts[word] != row[word]:
-                            logging.error('Word {} appears in the file {} times, and is in the df {} times'
-                                          .format(word, counts[word], row[word]))
+                            logging.error(
+                                'Word {} appears in the file {} times, and is in the df {} times'
+                                .format(word, counts[word], row[word]))
                             logging.error('Row:\n{}'.format(row))
                             return False
     except KeyError:
@@ -186,13 +195,16 @@ def get_total_word_count(folder_name):
     twc_dict = {}
     logging.info('Getting the total word count in {}'.format(folder_name))
     with cd(folder_name):
-        for data_class in tqdm([item for item in list_dir() if os.path.isdir(item)],
-                               desc='Classes processed', leave=False,
-                               file=sys.stdout, unit='class'):
+        for data_class in tqdm(
+            [item for item in list_dir()
+             if os.path.isdir(item)], desc='Classes processed', leave=False,
+                file=sys.stdout, unit='class'):
             logging.debug('get_total_word_cound | Processing class "{}"...'
                           .format(data_class))
             with cd(data_class):
-                for filename in [item for item in list_dir() if os.path.isfile(item)]:
+                for filename in [
+                        item for item in list_dir() if os.path.isfile(item)
+                ]:
                     temp_word_dict = get_word_dict(filename)
                     # twc_dict: {'word': [total_count, occured_in_N_files]}
                     for word, count in temp_word_dict.items():
@@ -217,26 +229,27 @@ def process_data_folder(folder_name, class_dict, popular_words):
     '''
     # We have the same return_cols for every file
     return_cols = np.array(['document_id', 'prediction_class'] +
-                           list(popular_words),
-                           dtype=np.dtype('str'))
+                           list(popular_words), dtype=np.dtype('str'))
     return_vals = []
     logging.info('Parsing the folder {}'.format(folder_name))
     with cd(folder_name):
-        for data_class in tqdm([item for item in list_dir() if os.path.isdir(item)],
-                               desc='Classes processed', leave=False,
-                               file=sys.stdout, unit='class'):
+        for data_class in tqdm(
+            [item for item in list_dir()
+             if os.path.isdir(item)], desc='Classes processed', leave=False,
+                file=sys.stdout, unit='class'):
             logging.debug('process_data_folder | Processing class "{}"...'
                           .format(data_class))
             with cd(data_class):
-                for filename in [item for item in list_dir() if os.path.isfile(item)]:
-                    temp_word_dict = get_word_dict_proper(filename,
-                                                          popular_words)
+                for filename in [
+                        item for item in list_dir() if os.path.isfile(item)
+                ]:
+                    temp_word_dict = get_word_dict_proper(
+                        filename, popular_words)
                     # Creating an array of values
-                    return_vals.append(np.array([filename, class_dict[data_class]] +
-                                                list(temp_word_dict.values()),
-                                                dtype=np.dtype('int')))
-    return (np.array(return_cols),
-            np.array(return_vals))
+                    return_vals.append(
+                        np.array([filename, class_dict[data_class]] + list(
+                            temp_word_dict.values()), dtype=np.dtype('int')))
+    return (np.array(return_cols), np.array(return_vals))
 
 
 # %% Reading the files - getting the popular words
@@ -248,7 +261,6 @@ if 'test' not in list_dir('data') or 'train' not in list_dir('data'):
     sys.exit('Bad structure of the data folder. '
              'Make sure that the data folder contains "test" and "train" dirs.')
 
-
 with cd('data'):
     logging.info('Generating classes...')
     class_dict = generate_classes('train')
@@ -257,30 +269,28 @@ with cd('data'):
     twc_train = get_total_word_count('train')
 
 # %% Transforming the data - selecting the most common words
-NUMBER_TOP_WORDS = 10_000
+NUMBER_TOP_WORDS = 10000
 
 # Making the 2d array from a dictionary
 wordlist = [[w, v[0], v[1]] for (w, v) in twc_train.items()]
 word_arr = np.stack(tuple(wordlist), axis=0)  # vertical stack
 
 # Making a df out of it
-word_df = pd.DataFrame(data=word_arr,
-                       columns=('words', 'count', 'count_files'))
+word_df = pd.DataFrame(data=word_arr, columns=('words', 'count', 'count_files'))
 
 # Change the cell types so we can properly compare them
 word_df['count'] = pd.to_numeric(word_df['count'])
 word_df['count_files'] = pd.to_numeric(word_df['count_files'])
 
 # Sort the values
-word_df.sort_values(by=['count', 'count_files'],
-                    axis=0, inplace=True, ascending=False)
+word_df.sort_values(by=['count', 'count_files'], axis=0, inplace=True,
+                    ascending=False)
 
 # Selecting the top words
 word_df = word_df[:NUMBER_TOP_WORDS]
 
 logging.info('Selected {} most popular words:'.format(NUMBER_TOP_WORDS))
 display(word_df)
-
 
 # Resetting index to be tidy
 uwords_series = word_df['words'].reset_index(drop=True)
@@ -294,12 +304,11 @@ display(uwords_df)
 
 with cd('data'):
     logging.info('Getting the train data...')
-    train_cols, train_vals = process_data_folder(
-        'train', class_dict, uwords_df.values[:, 0])
+    train_cols, train_vals = process_data_folder('train', class_dict,
+                                                 uwords_df.values[:, 0])
     logging.info('Getting the test data...')
-    test_cols, test_vals = process_data_folder(
-        'test', class_dict, uwords_df.values[:, 0])
-
+    test_cols, test_vals = process_data_folder('test', class_dict,
+                                               uwords_df.values[:, 0])
 
 logging.info('Train data sample:')
 logging.info('Columns:')
@@ -312,7 +321,6 @@ logging.info('Columns:')
 display(pd.DataFrame(test_cols).T)
 logging.info('Values:')
 display(pd.DataFrame(test_vals[0]).T)
-
 
 # %% Transforming the data - Making dataframes
 
@@ -331,25 +339,29 @@ display(test_df)
 # Checking
 PCT_TO_CHECK = 0.10
 
-logging.info('Checking the train_df ({} rows): {}!'
-             .format(int(PCT_TO_CHECK * train_df.shape[0]),
-                     'Passed the check'
-                     if check_df(train_df, class_dict, coverage_pct=PCT_TO_CHECK)
-                     else 'Didn\'t pass the check!'))
+logging.info(
+    'Checking the train_df ({}/{} rows): {}!'.format(
+        int(PCT_TO_CHECK * train_df.shape[0]),
+        train_df.shape[0],
+        'Passed the check'
+        if check_df(train_df, class_dict, coverage_pct=PCT_TO_CHECK) else
+        'Didn\'t pass the check!',
+    ),
+)
 
-logging.info('Checking the test_df ({} rows): {}!'
-             .format(int(PCT_TO_CHECK * test_df.shape[0]),
-                     'Passed the check'
-                     if check_df(test_df, class_dict,
-                                 df_type='test', coverage_pct=PCT_TO_CHECK)
-                     else 'Didn\'t pass the check!'))
-
+logging.info('Checking the test_df ({}/{} rows): {}!'.format(
+    int(PCT_TO_CHECK * test_df.shape[0]),
+    test_df.shape[0],
+    'Passed the check'
+    if check_df(test_df, class_dict, df_type='test', coverage_pct=PCT_TO_CHECK)
+    else 'Didn\'t pass the check!',
+))
 
 # %% Saving the results
 
 # Change this at will
 options = {
-    'save': False,  # Hopefully this will make the notebook viewable on github
+    'save': True,
     'archive': True,
     'save_folder': 'dataframes',
     'train_fname': 'train_df',
@@ -364,36 +376,34 @@ if options['save']:
     # Generating options that we can pass in a df's save function
     options = {
         'train': {
-            'path_or_buf': os.path.join(
-                options['save_folder'],
-                (
-                    ('{}.csv.gz' if options['archive'] else '{}.csv')
-                    .format(options['train_fname'])
-                )
-            ),
-            'compression': 'gzip' if options['archive'] else None
+            'path_or_buf':
+                os.path.join(options['save_folder'],
+                             (('{}.csv.gz' if options['archive'] else '{}.csv')
+                              .format(options['train_fname']))),
+            'compression':
+                'gzip' if options['archive'] else None
         },
-
         'test': {
-            'path_or_buf': os.path.join(
-                options['save_folder'],
-                (
-                    ('{}.csv.gz' if options['archive'] else '{}.csv')
-                    .format(options['test_fname'])
-                )
-            ),
-            'compression': 'gzip' if options['archive'] else None
+            'path_or_buf':
+                os.path.join(options['save_folder'],
+                             (('{}.csv.gz' if options['archive'] else '{}.csv')
+                              .format(options['test_fname']))),
+            'compression':
+                'gzip' if options['archive'] else None
         }
     }
 
     # Train df
-    logging.info('Saving the train DF to "{}"...'
-                 .format(options['train']['path_or_buf']))
+    logging.info(
+        'Saving the train DF to "{}"...'
+        .format(options['train']['path_or_buf']),
+    )
     train_df.to_csv(**options['train'])
     logging.info('Finished saving the train DF')
 
     # Test df
-    logging.info('Saving the test DF to "{}"...'
-                 .format(options['test']['path_or_buf']))
-    train_df.to_csv(**options['test'])
+    logging.info(
+        'Saving the test DF to "{}"...'.format(options['test']['path_or_buf']),
+    )
+    test_df.to_csv(**options['test'])
     logging.info('Finished saving the test DF')
